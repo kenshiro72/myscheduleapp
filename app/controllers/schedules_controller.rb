e0ec26357f2325a5315f50ed9c3730258b19214d
平賀絢史朗 <hiragakenshiro@hiragaayashirounonotobukkukonpyuta.local>
2024-05-19 01:31:01 +0900

    showアクションの修正

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -20,9 +20,7 @@ class SchedulesController < ApplicationController
   end
 
   def show
-    def show
-      @schedule = Schedule.find(params[:id])
-    end
+    @schedule = Schedule.find(params[:id])
   end
 
   def edit
